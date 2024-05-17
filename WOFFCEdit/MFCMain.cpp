c40cc90523c9a9326d7927d583260e7d428d8d12
TediShopov <tedikatana@abv.bg>
2024-05-17 19:55:18 +0100

    Reordering members in tool main

--- WOFFCEdit/MFCMain.cpp
+++ WOFFCEdit/MFCMain.cpp
@@ -34,11 +34,11 @@ BOOL MFCMain::InitInstance()
 
 	//get the rect from the MFC window so we can get its dimensions
 	m_toolHandle = m_frame->m_DirXView->GetSafeHwnd();				//handle of directX child window
-	m_frame->m_DirXView->GetClientRect(&WindowRECT);
+	m_frame->m_DirXView->GetClientRect(&m_windowRect);
 
 	//Assign window dimensions
-	m_width = WindowRECT.Width();
-	m_height = WindowRECT.Height();
+	m_width = m_windowRect.Width();
+	m_height = m_windowRect.Height();
 
 	//Initialize tools
 	m_ToolSystem.onActionInitialise(m_toolHandle, m_width, m_height);
